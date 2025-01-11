ad7709c8ac8b8ac721869a0563d022a41d801a92
Velan Thever <thevarvelan@gmail.com>
2025-01-11 17:33:30 +0530

    next folder removed

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/:all*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-store, no-cache, must-revalidate", // No caching for SSR pages
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;

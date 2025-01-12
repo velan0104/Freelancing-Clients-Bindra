ea8456e18b912bb049a275ada2a4873701cb24be
Velan Thever <thevarvelan@gmail.com>
2025-01-12 12:32:18 +0530

    Next folder removed

--- .next/core-js-master/tests/compat/rhino-adapter.mjs
+++
@@ -1,3 +0,0 @@
-const [path] = argv._;
-
-await $`java -jar ${ path } -require tests/compat/rhino-runner.js`;
--- .next/core-js-master/tests/eslint/runner.mjs
+++
@@ -1,3 +0,0 @@
-process.env.ESLINT_USE_FLAT_CONFIG = true;
-
-await $`TIMING=1 eslint --config ./tests/eslint/eslint.config.js ./`;
